26c86944749c03bed03972c96bc4261bcaeb5c2a
annserge <annserge@rambler.ru>
2016-04-08 14:56:28 +0300

    Площад квадрата и прям-к вычисляются в методах классов

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,12 +7,11 @@ public class MyFirstProgram {
     hello("user");
     hello("Anna");
 
-    double l=5;
-    System.out.println("Полщадь квадрата со стороной "+ l + " = " +area(l));
+    Square s=new Square(5);
+    System.out.println("Площадь квадрата со стороной "+ s.l + " = " + s.area());
 
-    double a=4;
-    double b=6;
-    System.out.println("Полщадь прямоугольника со сторонами " + a + " и " + b + " = " +area(a,b));
+    Rectangle r = new Rectangle(4,6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " +r.area());
 
   }
 
@@ -20,12 +19,4 @@ public class MyFirstProgram {
     System.out.println("Hello, " + somebody + "!");
   }
 
-  public static double area(double len) {
-    return len*len;
-  }
-
-  public static double area(double a, double b) {
-    return a*b;
-  }
-	
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Anna on 08.04.2016.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+  public double area() {
+    return this.l * this.l;
+  }
+}
