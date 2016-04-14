76f3ba717e3af0e7069b3d6c171c12f5f94f52fb
annserge <annserge@rambler.ru>
2016-04-14 18:50:56 +0300

    Добавлены зависимости TestNG для Gradle

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Anna on 14.04.2016.
+ */
+public class SquareTests {
+
+  @Test
+  public void testArea() {
+    Square s5 = new Square(5);
+    Assert.assertEquals(s5.area(), 25.0);
+    Square s12 = new Square(12);
+    Assert.assertEquals(s12.area(), 144.0);
+    Square s0 = new Square(0);
+    Assert.assertEquals(s0.area(), 0.0);
+  }
+}
