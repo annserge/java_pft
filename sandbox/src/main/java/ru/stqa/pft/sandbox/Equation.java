cd9b7e0c442378ad278706503bf4042448bf3d99
annserge <annserge@rambler.ru>
2016-04-21 16:04:26 +0300

    Подсчет корней кв уравнения (if/else) и тесты

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -0,0 +1,35 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Anna on 21.04.2016.
+ */
+public class Equation {
+
+  private double a;
+  private double b;
+  private double c;
+
+  private int n;
+
+  public Equation (double a, double b, double c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+
+    double d = b*b - 4*a*c;
+
+    if (d > 0) {
+      n = 2;
+    } else {
+      if (d == 0) {
+        n = 1;
+      } else {
+        n = 0;
+      }
+    }
+  }
+
+  public int rootNumber() {
+    return n;
+  }
+}
---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
@@ -0,0 +1,29 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Anna on 21.04.2016.
+ */
+
+public class EquationTests {
+
+  @Test
+  public void test0() {
+    Equation e = new Equation(1,1,1);
+    Assert.assertEquals(e.rootNumber(), 0);
+  }
+
+  @Test
+  public void test1() {
+    Equation e = new Equation(1,2,1);
+    Assert.assertEquals(e.rootNumber(), 1);
+  }
+
+  @Test
+  public void test2() {
+    Equation e = new Equation(1,5,6);
+    Assert.assertEquals(e.rootNumber(), 2);
+  }
+}
