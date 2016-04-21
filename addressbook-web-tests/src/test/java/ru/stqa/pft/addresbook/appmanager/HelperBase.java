34b5ecd2d89abc7ce75cf3306c35636c63aed80d
annserge <annserge@rambler.ru>
2016-04-21 18:28:54 +0300

    Проверка: если поле заполнено как надо по дефолту, не трогать

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/appmanager/HelperBase.java
@@ -23,8 +23,13 @@ public class HelperBase {
 
   protected void type(By locator, String text) {
     click(locator);
-    wd.findElement(locator).clear();
-    wd.findElement(locator).sendKeys(text);
+    if (text != null) {
+      String existingText = wd.findElement(locator).getAttribute("value");
+      if (! text.equals(existingText)) {
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+      }
+    }
   }
 
   public boolean isAlertPresent() {
