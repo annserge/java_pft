1d7d53664f9a2ab2f4b679353d1e10a74b6d1b13
annserge <annserge@rambler.ru>
2016-04-25 13:31:49 +0300

    Оптимизированы переходы между страницами
    методы:
    gotoGroupPage
    gotoAddNewPage
    returnToHomePage

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/appmanager/NavigationHelper.java
@@ -13,11 +13,25 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void gotoGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new")) ) {
+      return;
+    }
     click(By.linkText("groups"));
+    //переход по линку groups, только пока не: найден заголовок с тегом h1 и именем Groups плюс кнопка создания группы с имененм new
   }
 
-  public void gotoAddNewPage() { click(By.linkText("add new"));  }
+  public void gotoAddNewPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Edit / add address book entry")) {return;}
+    click(By.linkText("add new"));
+  }
 
-  public void returnToHomePage() { click(By.linkText("home"));  }
-
+  public void returnToHomePage() {
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    }
+    click(By.linkText("home"));
+  }
 }
