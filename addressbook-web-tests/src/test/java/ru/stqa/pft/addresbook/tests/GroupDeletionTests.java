35fa247dd8c8bae8358789dbd66cea64f0665f8a
annserge <annserge@rambler.ru>
2016-04-25 14:25:57 +0300

    Проверка и обеспечение выполнения предусловий

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/GroupDeletionTests.java
@@ -1,12 +1,18 @@
 package ru.stqa.pft.addresbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addresbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() {
     app.getNavigationHelper().gotoGroupPage();
+
+    //если нет группы, то создать ее
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/GroupModificationTests.java
@@ -11,6 +11,11 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification () {
     app.getNavigationHelper().gotoGroupPage();
+
+    //если нет группы, то создать ее
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
