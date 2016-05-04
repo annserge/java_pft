e0bb37bc3cd0acd1b3c0e9ff0449a3d01af17ed4
annserge <annserge@rambler.ru>
2016-05-04 09:37:36 +0300

    Сравнение коллекций при + групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/model/GroupData.java
@@ -1,27 +1,31 @@
 package ru.stqa.pft.addresbook.model;
 
 public class GroupData {
-  private final String id;
+  private int id;
   private final String name;
   private final String header;
   private final String footer;
 
   public GroupData(String name, String header, String footer) {
-    this.id = null;
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
-  public GroupData(String id, String name, String header, String footer) {
+  public GroupData(int id, String name, String header, String footer) {
     this.id = id;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
-  public String getId() {
+  public int getId() {
     return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
   }
 
   public String getName() {
@@ -43,14 +47,14 @@ public class GroupData {
 
     GroupData groupData = (GroupData) o;
 
-    if (id != null ? !id.equals(groupData.id) : groupData.id != null) return false;
+    if (id != groupData.id) return false;
     return name != null ? name.equals(groupData.name) : groupData.name == null;
 
   }
 
   @Override
   public int hashCode() {
-    int result = id != null ? id.hashCode() : 0;
+    int result = id;
     result = 31 * result + (name != null ? name.hashCode() : 0);
     return result;
   }
