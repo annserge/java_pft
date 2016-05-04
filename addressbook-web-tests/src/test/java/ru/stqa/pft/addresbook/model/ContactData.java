c2fe34918206e9dd1d9dd446466288b0638ccc41
annserge <annserge@rambler.ru>
2016-05-04 13:01:34 +0300

    Сравнение списков при - контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/model/ContactData.java
@@ -29,6 +29,7 @@ public class ContactData {
     return firstName;
   }
 
+
   public String getLastName() {
     return lastName;
   }
@@ -64,4 +65,32 @@ public class ContactData {
   public String getGroup() {
     return group;
   }
+
+  @Override
+  public String toString() {
+    return "ContactData{" +
+            "firstName='" + firstName + '\'' +
+            ", lastName='" + lastName + '\'' +
+            '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    ContactData that = (ContactData) o;
+
+    if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
+    return lastName != null ? lastName.equals(that.lastName) : that.lastName == null;
+
+  }
+
+  @Override
+  public int hashCode() {
+    int result = firstName != null ? firstName.hashCode() : 0;
+    result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+    return result;
+  }
 }
+
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/ContactDeletionTests.java
@@ -28,5 +28,8 @@ public class ContactDeletionTests extends TestBase {
 
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after);
   }
 }
