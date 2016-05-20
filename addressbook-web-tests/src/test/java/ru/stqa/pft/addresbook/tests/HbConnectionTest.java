ad064c3c7621ec2bd029df64ff2661b2aa5288f6
annserge <annserge@rambler.ru>
2016-05-20 12:12:30 +0300

    no orm connection

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/HbConnectionTest.java
@@ -0,0 +1,50 @@
+package ru.stqa.pft.addresbook.tests;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addresbook.model.GroupData;
+
+import java.util.List;
+
+/**
+ * Created by Anna on 20.05.2016.
+ */
+public class HbConnectionTest {
+
+  private SessionFactory sessionFactory;
+
+  @BeforeClass
+  protected void setUp() throws Exception {
+    // A SessionFactory is set up once for an application!
+    final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure() // configures settings from hibernate.cfg.xml
+            .build();
+    try {
+      sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+      // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+      // so destroy it manually.
+      StandardServiceRegistryBuilder.destroy( registry );
+    }
+  }
+
+  @Test
+  public void testHbConnection() {
+    Session session = sessionFactory.openSession();
+    session.beginTransaction();
+    List<GroupData> result = session.createQuery( "from GroupData" ).list();
+    for ( GroupData group : result ) {
+      System.out.println(group);
+    }
+    session.getTransaction().commit();
+    session.close();
+
+  }
+}
